fab0296b77e724f809b1e96ac59c048d89b46326
chirag gaur <chiragwalker@gmail.com>
2023-07-25 16:30:19 +0530

    dono task with revision

--- block-BJaair/code/script.js
+++ block-BJaair/code/script.js
@@ -1,34 +1,74 @@
-let notice = document.querySelector('.noticeBoard');
 let form = document.querySelector('form');
+let ul =  document.querySelector('.noticeBoard');
+let cardsData =  JSON.parse(localStorage.getItem('cards')) || [];//object form data input
 
-form.addEventListener('submit',handleEvent);
 
-function handleEvent(event){
-    event.preventDefault();// prevent default refresh
-    let indivisualNotice = document.createElement('div');
-    indivisualNotice.classList = "noticeBox";
-    let noticeAnchor = document.createElement('input');
-      noticeAnchor.id = "titleText";
-      noticeAnchor.type = 'text';
-    let noticeCategory = document.createElement('input') ;
-    noticeCategory.id = "titleCategory";
-    noticeCategory.type = 'text';
+// form events
+form.addEventListener('submit',(event)=>{
+     event.preventDefault(); // prevent default refresh
+     let title = event.target.elements.title.value;
+     let category = event.target.elements.title.value;
+     cardsData.push({title,category});
+// local storage support
+    localStorage.setItem('cards',JSON.stringify(cardsData));
+     createUI(cardsData,ul);
+      
+})
 
-     noticeAnchor.value = form.elements.title.value;
-     noticeCategory.value = form.elements.category.value;
+//handleEdit and update cardsdata
 
-     indivisualNotice .append(noticeCategory,noticeAnchor);
-    notice.append(indivisualNotice);
+function handleEdit(event,info,id,label){
+    let elm = event.target
+    let input = document.createElement('input');
+       input.type = 'text';
+       input.value = info;
 
-    // ondouble click change
+    input.addEventListener('keyup',(e) => {
+        if(e.keyCode === 13){
+            let  updatedValue = e.target.value;
+            cardsData[id][label] = updatedValue;
+            createUI();
+            localStorage.setItem('cards',JSON.stringify(cardsData));
+        }
+    });   
 
-    noticeAnchor.addEventListener('dblclick',editCategory); // doubt edit ob double click
-    
-    function editCategory(event){
-        noticeAnchor.value = event.target.value;
-        indivisualNotice.append(noticeAnchor);
-        console.log(noticeAnchor);
-
-    };
- 
+    input.addEventListener('blur',(e) => {  
+            let  updatedValue = e.target.value;
+            cardsData[id][label] = updatedValue;
+            createUI();
+            localStorage.setItem('cards',JSON.stringify(cardsData));
+    });   
+    let parent = event.target.parentElement;
+    parent.replaceChild(input,elm);
+  
 }
+
+
+
+
+
+
+// creating ui from data 
+
+function createUI(data = cardsData,root = ul){
+     root.innerHTML = '';
+    let fragment = new DocumentFragment();
+    data.forEach((cardInfo,index) => {
+       let li = document.createElement('li');
+        li.id = 'list';
+       let p = document.createElement('p');
+       p.id = 'category';
+       p.innerText = cardInfo.category;
+
+      p.addEventListener('dblclick',(event)=> handleEdit(event,cardInfo.category,index,'category')); // pass arguments in callback       
+       let h2 = document.createElement('h2');
+       h2.id = 'title';
+       h2.innerText = cardInfo.title;
+       h2.addEventListener('dblclick',(event)=> handleEdit(event,cardInfo.title,index,'title')); // pass arguments in callback 
+       li.append(p,h2);
+       fragment.appendChild(li);
+    })
+    root.appendChild(fragment);
+}
+
+createUI(cardsData,ul);
